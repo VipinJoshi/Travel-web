759c1a84b9e0b829b0b9f40eb113db34e0554b29
VipinJoshi <vipiswaiting@live.com>
2019-02-05 16:26:01 +0530

    initial commit with no code

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/Travel-web'));
+app.use(express.static('./dist/travel-web'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/Travel-web/index.html'));
+res.sendFile(path.join(__dirname,'/dist/travel-web/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
